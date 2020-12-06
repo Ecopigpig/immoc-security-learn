1a14fae0f1c3795e005d24f1ee1aa022df727c5f
linxinting <Ecopipig@163.com>
2020-12-06 11:31:12 +0800

    4-2 Security基本原理完成

--- imooc-security-demo/src/main/java/com/imooc/web/wiremock/MockServer.java
+++ imooc-security-demo/src/main/java/com/imooc/web/wiremock/MockServer.java
@@ -2,6 +2,7 @@ package com.imooc.web.wiremock;
 
 import com.github.tomakehurst.wiremock.client.WireMock;
 import org.apache.commons.io.FileUtils;
+import org.apache.commons.lang.StringUtils;
 import org.springframework.core.io.ClassPathResource;
 
 import java.io.IOException;
@@ -15,11 +16,17 @@ import java.io.IOException;
 public class MockServer {
     public static void main(String[] args) throws IOException {
         //指定已经启动完毕的wireMock.jar包的端口
-        WireMock.configureFor(9090);
+        WireMock.configureFor(8062);
         WireMock.removeAllMappings();
-        ClassPathResource resource = new ClassPathResource("/mock/resource/01.txt");
-        String content = FileUtils.readFileToString(resource.getFile());
-        WireMock.stubFor(WireMock.get(WireMock.urlPathEqualTo("/order/1"))
-                .willReturn(WireMock.aResponse().withBody("{\"id\":1}").withStatus(200)));
+        mock("/order/1","01");
+        mock("/order/2","02");
+
+    }
+
+    private static void mock(String url,String fileName) throws IOException {
+        ClassPathResource resource = new ClassPathResource("/resources/mock/resource/"+fileName+".txt");
+        String content = StringUtils.join(FileUtils.readLines(resource.getFile(),"UTF-8").toArray(),"\n");
+        WireMock.stubFor(WireMock.get(WireMock.urlPathEqualTo(url))
+                .willReturn(WireMock.aResponse().withBody(content).withStatus(200)));
     }
 }
